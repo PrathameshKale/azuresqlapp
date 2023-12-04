f4874b00d599f8c17fc86aa5f0b886d581fbbb01
Prathamesh Kale <pkale39@gmail.com>
2023-12-04 16:04:00 +0530

    Updated app for dependency injection

--- sqlapp/Program.cs
+++ sqlapp/Program.cs
@@ -1,8 +1,19 @@
+using sqlapp.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+//var connectionString = "Endpoint=https://appconfig1000233.azconfig.io;Id=E0j2-l8-s0:ywglWz9a/tngma/7q5/S;Secret=QA16TLCFkzE7j+BmvBnm/zlC2VJ/X4HqViAuA/PMHeQ=";
+
+//builder.Host.ConfigureAppConfiguration(builder =>
+//{
+//    //Connect to your App Config Store using the connection string
+//    builder.AddAzureAppConfiguration(connectionString);
+//});
+
+builder.Services.AddTransient<IProductService, ProductService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
